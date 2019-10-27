70e2fecee7dee3c587ca4fe720f0b7f3f15ebf80
Parker Damon <pdamon@mines.edu>
2019-10-26 18:10:15 -0600

    all done with opencv proof of concept

--- opencvtest/test.py
+++ opencvtest/test.py
@@ -1,30 +1,46 @@
 import cv2
-import numpy as np
-from matplotlib import pyplot as plot
 
 
 def main():
-    f = cv2.VideoCapture("pong.mp4")
+    with open("outputs.txt", "w+") as out:
+        f = cv2.VideoCapture("pong.mp4")
 
-    if not f.isOpened():
-        print("error opening file")
+        if not f.isOpened():
+            print("error opening file")
 
-    while f.isOpened():
-        ret, frame = f.read()
-        if ret:
-            grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            thresh = cv2.threshold(grayframe, 200, 255, cv2.THRESH_BINARY)
+        while f.isOpened():
+            ret, frame = f.read()
+            if ret:
+                grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                ret, thresh = cv2.threshold(grayframe, 240, 255, cv2.THRESH_BINARY)
 
-            cv2.imshow("Frame", thresh)
+                contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            if cv2.waitKey(25) & 0xFF == ord('q'):
+                if len(contours) != 0:
+                    c = max(contours, key=cv2.contourArea)
+                    moments = cv2.moments(c)
+
+                    if moments['m00'] != 0:
+                        x = int(moments['m10'] / moments['m00'])
+                        y = int(moments['m01'] / moments['m00'])
+
+                        cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
+
+                        out.write("x:{} y:{}\n".format(x, y))
+
+                    # cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
+                    cv2.imshow("With Contours", frame)
+
+                    if cv2.waitKey(25) & 0xFF == ord('q'):
+                        break
+            else:
                 break
-        else:
-            break
 
-    f.release()
+        f.release()
 
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
+
+    out.close()
 
 
 if __name__ == '__main__':
