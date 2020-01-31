4bbcfa82a54ff1cfaad9025715df99c4738647f3
Parker Damon <pcdamon@pm.me>
2020-01-30 20:05:54 -0700

    delete non-necessary parts for opencv headless testing

--- opencvtest/test.py
+++ opencvtest/test.py
@@ -8,11 +8,11 @@ def main():
         if not f.isOpened():
             print("error opening file")
 
-        frame_width = int(f.get(3))
-        frame_height = int(f.get(4))
+        # frame_width = int(f.get(3))
+        # frame_height = int(f.get(4))
 
-        output = cv2.VideoWriter("example.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30,
-                                 (frame_width, frame_height))
+        # output = cv2.VideoWriter("example.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30,(frame_width, frame_height))
+
         while f.isOpened():
             ret, frame = f.read()
             if ret:
@@ -29,23 +29,23 @@ def main():
                         x = int(moments['m10'] / moments['m00'])
                         y = int(moments['m01'] / moments['m00'])
 
-                        cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
+                        # cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
 
                         out.write("x:{} y:{}\n".format(x, y))
 
                     # cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
-                    output.write(frame)
-                    cv2.imshow("With Contours", frame)
+                    # output.write(frame)
+                    # cv2.imshow("With Contours", frame)
 
-                    if cv2.waitKey(25) & 0xFF == ord('q'):
-                        break
+                    # if cv2.waitKey(25) & 0xFF == ord('q'):
+                        # break
             else:
                 break
 
         f.release()
-        output.release()
+        # output.release()
 
-        cv2.destroyAllWindows()
+        # cv2.destroyAllWindows()
 
     out.close()
 
